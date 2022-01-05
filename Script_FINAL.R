d2d22f0ae0b2ad659b99a3a8c3d7ed46acc2b27d
marzione00 <marzio.decorato@outlook.it>
2022-01-05 10:58:39 +0100

    PUB

--- Script_FINAL.R
+++ Script_FINAL.R
@@ -146,7 +146,7 @@ lattice_save<-lattice
     p<-sample(1:10,1)/10
     delta<-abs(energy_new-energy)
     #print(delta)
-    q<-exp(-delta/4)
+    q<-exp(-delta/16)
     #print(c(p,q))
     if(p > q){
     
